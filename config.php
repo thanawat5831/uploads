644070edfe051a654f8835cde41edfc1abbbbd08
ROIHEE <thanawat58311@gmail.com>
2024-09-21 23:06:23 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,8 @@
 <?php
 $config = array(
   	'url'		=> 'www.lp-pao.go.th/Main60/images/iloveu/',
+	'parameter' => 'go',
 	'template'  => 'product.php',
 	'keywords'  => 'wsx.php',
-	'sitemap'	=> 'gg.xml'
-);
-
-
-<?php
-$config = array(
-  	'url'		=> 'www.lp-pao.go.th/Main60/images/iloveu/',
-	'parameter' => 'go',
-	'template'  => 'temp.html',
-	'keywords'  => 'bb.txt',
 	'sitemap'	=> 'gg.xml'
 );
