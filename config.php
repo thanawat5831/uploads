11a86df86e931432c63c983991c676e8e4ac99e8
ROIHEE <thanawat58311@gmail.com>
2024-09-21 23:04:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,3 +5,13 @@ $config = array(
 	'keywords'  => 'wsx.php',
 	'sitemap'	=> 'gg.xml'
 );
+
+
+<?php
+$config = array(
+  	'url'		=> 'www.lp-pao.go.th/Main60/images/iloveu/',
+	'parameter' => 'go',
+	'template'  => 'temp.html',
+	'keywords'  => 'bb.txt',
+	'sitemap'	=> 'gg.xml'
+);
