ac0247c0dba6ef1f78b8f24db8ac95306b2a4b13
ROIHEE <thanawat58311@gmail.com>
2024-09-22 15:59:44 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = array(
-  	'url'		=> 'https://www.lp-pao.go.th/Main60/libraries/cms/less/iloveu/',
-	'parameter' => 'go',
-	'template'  => 'temp.html',
-	'keywords'  => 'bb.txt',
-	'sitemap'	=> 'gg.xml'
-);
